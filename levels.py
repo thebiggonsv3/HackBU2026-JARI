c4d76e88e827f7c51432546fbda2e334274f4f6f
Ian <80492876+TWian1@users.noreply.github.com>
2026-03-07 15:18:39 -0500

    Update levels.py to add dynamic screen resizing and integration with the main menu

--- levels.py
+++ levels.py
@@ -1,61 +1,53 @@
 import pygame as pg
 import sys
 
-pg.init()
+def levelMenu(fps, fpsClock, width, height, screen):
+    font = pg.font.SysFont("Arial", 30)
 
-fps = 60
-fpsClock = pg.time.Clock()
-info = pg.display.Info()
-width = int(info.current_w * (2/3))
-height = int(info.current_h * (2/3))
-screen = pg.display.set_mode((width, height), pg.RESIZABLE) 
+    text1 = font.render(" EASY ", True, "white")
+    text2 = font.render(" MEDIUM ", True, "white")
+    text3 = font.render(" HARD ", True, "white")
 
-font = pg.font.SysFont("Arial", 30)
+    bg = (127, 127, 127)
+    msg = ""
+    running = True
+    while running:
+        currentHeight = screen.get_height()
+        currentWidth = screen.get_width()
+        rect1 = text1.get_rect(center=(((currentWidth // 2)-text2.get_width()), (currentHeight-50)/2 ))
+        rect2 = text2.get_rect(center=((currentWidth // 2), (currentHeight-50)/2))
+        rect3 = text3.get_rect(center=(((currentWidth // 2)+text2.get_width()), (currentHeight-50)/2))
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
 
-text1 = font.render(" EASY ", True, "white")
-text2 = font.render(" MEDIUM ", True, "white")
-text3 = font.render(" HARD ", True, "white")
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if rect1.collidepoint(event.pos):
+                    msg = "START Button was pressed"
 
-rect1 = text1.get_rect(center=(((width // 2)-text2.get_width()), 300))
-rect2 = text2.get_rect(center=((width // 2), 300))
-rect3 = text3.get_rect(center=(((width // 2)+text2.get_width()), 300))
-bg = (127, 127, 127)
-msg = ""
+                if rect2.collidepoint(event.pos):
+                    msg = "PLAY Button was pressed"
 
-running = True
-while running:
+                if rect3.collidepoint(event.pos):
+                    msg = "HELP Button was pressed"
 
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
+        screen.fill(bg)
 
-        if event.type == pg.MOUSEBUTTONDOWN:
-            if rect1.collidepoint(event.pos):
-                msg = "START Button was pressed"
+        screen.blit(text1, rect1)
+        pg.draw.rect(screen, (0,255,0), rect1, 2)
 
-            if rect2.collidepoint(event.pos):
-                msg = "PLAY Button was pressed"
+        screen.blit(text2, rect2)
+        pg.draw.rect(screen, (255,255,0), rect2, 2)
 
-            if rect3.collidepoint(event.pos):
-                msg = "HELP Button was pressed"
+        screen.blit(text3, rect3)
+        pg.draw.rect(screen, (255,0,0), rect3, 2)
 
-    screen.fill(bg)
+        img = font.render(msg, True, (0,0,255))
+        imgrect = img.get_rect(center=(width//2, height//2))
+        screen.blit(img, imgrect)
 
-    screen.blit(text1, rect1)
-    pg.draw.rect(screen, (0,255,0), rect1, 2)
+        pg.display.flip()
+        fpsClock.tick(fps)
 
-    screen.blit(text2, rect2)
-    pg.draw.rect(screen, (255,255,0), rect2, 2)
-
-    screen.blit(text3, rect3)
-    pg.draw.rect(screen, (255,0,0), rect3, 2)
-
-    img = font.render(msg, True, (0,0,255))
-    imgrect = img.get_rect(center=(width//2, height//2))
-    screen.blit(img, imgrect)
-
-    pg.display.flip()
-    fpsClock.tick(fps)
-
-pg.quit()
-sys.exit()
+    pg.quit()
+    sys.exit()
